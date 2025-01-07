60f8a0543d99463d448bd38adb0f8c2f41c9111b
NazarethL <91594663+NazarethL@users.noreply.github.com>
2025-01-07 18:56:50 +0000

    Inicio del learn Python para principiantes

---
+++ pythonParaPrincipiantes/program.py
@@ -0,0 +1,16 @@
+import os
+os.system('clear')
+import sys
+
+##print(sys.argv)
+##print(sys.argv[0]) # program name
+##print(sys.argv[1]) # first arg
+
+##print("Bienvenido al programa")
+##name = input("Introduce un nombre:")
+##print("Felicidades, " + name)
+
+print("Calculadora")
+first_number = input("Primer numero: ")
+second_number = input("Segundo numbero: ")
+print(first_number + second_number)
